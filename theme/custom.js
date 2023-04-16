dd13e61c4d9848a2dfd5b0231b3fea19cf6fc56b
weibing0.ma@gmail.com <weibing0.ma@gmail.com>
2023-04-16 20:49:18 +0800

    update

---
+++ theme/custom.js
@@ -0,0 +1,15 @@
+<script src="https://giscus.app/client.js"
+        data-repo="bertreyking/bertreyking.github.io"
+        data-repo-id="R_kgDOJOGO-g"
+        data-category="Announcements"
+        data-category-id="DIC_kwDOJOGO-s4CV0Ol"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="light"
+        data-lang="zh-CN"
+        crossorigin="anonymous"
+        async>
+</script>
